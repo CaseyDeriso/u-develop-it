d7f99c3557989e5157c8866b82a96083689a0e83
Casey <caseyderiso@gmail.com>
2020-12-16 11:27:51 -0600

    created CRUD database calls in server.js

--- server.js
+++ server.js
@@ -1,20 +1,58 @@
-const express = require('express');
+const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const sqlite3 = require("sqlite3").verbose();
 
 // middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-;
-// Routes
+// Connect to database
+const db = new sqlite3.Database("./db/election.db", (err) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log("connected to the election database.");
+});
 
+// // database calls
+// // get ALL candidates
+// db.all(`SELECT * FROM candidates`, (err, rows) => {
+//   console.log(rows);
+// });
+// //  GET a single candidates
+// db.get(`SELECT * FROM candidates WHERE id=1`, (err, row) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(row);
+// });
+// // delete a candidate
+// db.run(`DELETE FROM candidates WHERE id = ?`, 1, function (err, result) {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result, this, this.changes);
+// });
+// // Create a Candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+// Values (?,?,?,?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
+// // ES5 function, nor arrow function, to use 'this'
+// db.run(sql, params, function(err, result) {
+//   if (err) {
+//     console.log(err)
+//   }
+//   console.log(result, this.lastID)
+// })
 
 // Default response for any other request(Not Found) Catch all
 app.use((req, res) => {
   res.status(404).end();
-})
+});
 
-app.listen(PORT, () => {
-  console.log(`server is live on port ${PORT}`);
+db.on("open", () => {
+  app.listen(PORT, () => {
+    console.log(`server is live on port ${PORT}`);
+  });
 });
